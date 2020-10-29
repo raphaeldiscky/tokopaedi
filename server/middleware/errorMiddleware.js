05a116a8b56aa5e9221981e719a1ce8bdfa982c6
Raphael Discky <zundria.putra@gmail.com>
2020-10-29 18:06:01 +0700

    Create a redux store

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -7,7 +7,7 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode; //sometises we might get a 200 response, eventhough it's an error
   res.status(statusCode);
   res.json({
     message: err.message,
