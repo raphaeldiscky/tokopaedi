d49ffaed20dd4a263ddbd878507a8e23bfa0014c
Raphael Discky <zundria.putra@gmail.com>
2020-10-29 13:49:34 +0700

    Connect to database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
 const app = express();
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API is running...');
