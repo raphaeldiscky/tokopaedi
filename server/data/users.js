545c28c15b945ed254279ded7faa8a8238df3c38
Raphael Discky <zundria.putra@gmail.com>
2020-10-29 14:37:31 +0700

    Create data seeder script to import/destroy dummy data

---
+++ server/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10)
+  },
+  {
+    name: 'Messi',
+    email: 'messi@example.com',
+    password: bcrypt.hashSync('123456', 10)
+  }
+];
+
+export default users;
