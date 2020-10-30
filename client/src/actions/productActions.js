8083554731c614b99e9db7594b45fe944a1b50aa
Raphael Discky <zundria.putra@gmail.com>
2020-10-30 12:14:49 +0700

    Create productDetails reducer & action

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -1,7 +1,10 @@
 import {
   PRODUCT_LIST_REQUEST, // this actions
   PRODUCT_LIST_SUCCESS,
-  PRODUCT_LIST_FAIL
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants';
 import axios from 'axios';
 
@@ -18,6 +21,26 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message // message from errorHandler
+          : error.message
+    });
+  }
+};
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${id}`); // res.data
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message // message from errorHandler
