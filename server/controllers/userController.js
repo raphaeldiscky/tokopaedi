319d98145173795789319a94200ccc1456892d2e
Raphael Discky <zundria.putra@gmail.com>
2020-11-08 14:32:23 +0700

    Create user registration & password encryption

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,7 +7,7 @@ import generateToken from '../utils/generateToken.js';
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email: email });
+  const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
@@ -19,6 +19,36 @@ const authUser = asyncHandler(async (req, res) => {
   } else {
     res.status(401);
     throw new Error('Invalid email or password');
+  }
+});
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+  const user = await User.create({
+    //create = save in userModel.js
+    name,
+    email,
+    password
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
   }
 });
 
@@ -40,4 +70,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
