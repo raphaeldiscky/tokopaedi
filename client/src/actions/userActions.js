39537975b1ce193c9a969ec78e4b1040d8e7ce77
Raphael Discky <zundria.putra@gmail.com>
2020-11-08 20:30:34 +0700

    Show user in navbar & logout

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,7 +1,8 @@
 import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
-  USER_LOGIN_SUCCESS
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT
 } from '../constants/userConstants';
 import axios from 'axios';
 
@@ -36,4 +37,9 @@ export const login = (email, password) => async (dispatch) => {
           : error.message
     });
   }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
 };
