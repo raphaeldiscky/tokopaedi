3d464b0eb457cdd7a38209ab3852e95a798d5a04
Raphael Discky <zundria.putra@gmail.com>
2020-11-11 22:24:26 +0700

    Create get user by ID & Update user endpoints

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,4 +1,3 @@
-import e from 'express';
 import asyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 
@@ -18,7 +17,6 @@ const addOrderItems = asyncHandler(async (req, res) => {
   if (orderItems && orderItems.length === 0) {
     res.status(400);
     throw new Error('No order items');
-    return;
   } else {
     const order = new Order({
       orderItems,
