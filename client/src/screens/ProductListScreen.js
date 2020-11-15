0069791609d5c5bfaa068dc048ed6847d899030f
Raphael Discky <zundria.putra@gmail.com>
2020-11-15 17:30:55 +0700

    Create admin delete product

--- client/src/screens/ProductListScreen.js
+++ client/src/screens/ProductListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button, Modal, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { listProducts } from '../actions/productActions';
+import { listProducts, deleteProduct } from '../actions/productActions';
 
 const ProductListScreen = ({ history, match }) => {
   const dispatch = useDispatch();
@@ -17,6 +17,13 @@ const ProductListScreen = ({ history, match }) => {
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
 
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete
+  } = productDelete;
+
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
@@ -26,10 +33,10 @@ const ProductListScreen = ({ history, match }) => {
     } else {
       history.push('/login');
     }
-  }, [dispatch, history, userInfo]);
+  }, [dispatch, history, userInfo, successDelete]);
 
   const deleteHandler = (id) => {
-    // dispatch(deleteProduct(id));
+    dispatch(deleteProduct(id));
     setShow(false);
   };
 
@@ -47,6 +54,8 @@ const ProductListScreen = ({ history, match }) => {
           </Button>
         </Col>
       </Row>
+      {loadingDelete && <loader />}
+      {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
       {loading ? (
         <Loader />
       ) : error ? (
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,10 +2,15 @@ import express from 'express';
 const router = express.Router();
 import {
   getProductById,
-  getProducts
+  getProducts,
+  deleteProductById
 } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProductById);
 
 export default router;
