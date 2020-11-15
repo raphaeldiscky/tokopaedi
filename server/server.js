cda65245fb696f06fec720948cefcdb92e27999f
Raphael Discky <zundria.putra@gmail.com>
2020-11-15 22:12:20 +0700

    Create image upload config & endpoint

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
@@ -6,6 +7,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 const app = express();
@@ -19,11 +21,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+const __dirname = path.resolve(); // for ES module
+app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); // make upload folder static
 app.use(notFound);
 app.use(errorHandler);
 
