7494ffddd4784c08554f1a0424b68bd835d6b547
Raphael Discky <zundria.putra@gmail.com>
2020-11-18 18:23:58 +0700

    Ready for deployment

--- server/server.js
+++ server/server.js
@@ -21,10 +21,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json()); // accept json data in the body
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -36,6 +32,18 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve(); // for ES module
 app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); // make upload folder static
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
